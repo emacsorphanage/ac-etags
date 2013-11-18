015820a959905874426ae94a57e1bd0279c87d87
Syohei YOSHIDA <syohex@gmail.com>
2013-11-18 16:26:53 +0900

    Bump up version and update Changes

--- ac-etags.el
+++ ac-etags.el
@@ -4,7 +4,7 @@
 
 ;; Author: Syohei YOSHIDA <syohex@gmail.com>
 ;; URL: https://github.com/syohex/emacs-ac-etags
-;; Version: 0.01
+;; Version: 0.02
 ;; Package-Requires: ((auto-complete "1.4"))
 
 ;; This program is free software; you can redistribute it and/or modify
